e7f89e028b4c9a3678fbf9f82e72e8b1074f9e01
Zack Hartmann <zackhartmann@gmail.com>
2018-03-07 17:05:56 -0500

    Implemented Invalid Argument Exceptions

--- largest-series-product/largest-series-product.php
+++ largest-series-product/largest-series-product.php
@@ -5,11 +5,17 @@ class Series {
 	private $digits;
 
 	public function __construct($digits) {
+		if (preg_match("/\D/", $digits))
+			throw new InvalidArgumentException('Input must contain only Digits');
+
 		$this->digits = $digits;
 	}
 
 	public function largestProduct($width) {
 		$digits = $this->digits;
+		if ($width < 1 || $width > strlen($digits))
+			throw new InvalidArgumentException('Span cannot be Negative');
+
 		$high = 0;
 		for ($i = 0; $i <= strlen($digits) - $width; $i++) {
 			$product = 1;
