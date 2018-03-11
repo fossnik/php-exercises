abfd9ecebb71e9e663d52e4a4d79738384d66000
Zack Hartmann <zackhartmann@gmail.com>
2018-03-11 12:35:48 -0400

    Implemented Array_Flip in order to use Key Value Indices

--- sieve/sieve.php
+++ sieve/sieve.php
@@ -7,21 +7,20 @@
  */
 
 function sieve($input) {
-	$notPrime = [];
+	if ($input < 2)
+		return [];
 
-	foreach (range(3, $input) as $j) {
-		$k = $j;
-		while ($k < $j) {
-			$notPrime[$k] = true;
-			$k += $j;
+	$sieve = range(2, $input);
+	$sieve = array_flip($sieve);
+
+	foreach ($sieve as $k => $value) {
+		$tis = $k;
+		while ($tis <= $input) {
+			$tis += $k;
+			unset($sieve[$tis]);
 		}
 	}
 
-	$remit = [];
-	foreach (range(2, $input) as $l)
-		if ($notPrime[$l] == false)
-			array_push($remit, $l);
-
-	unset($remit[1]);
-	return $remit;
+	$remit = array_flip($sieve);
+	return array_values($remit);
 }
